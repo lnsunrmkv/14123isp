9bdafa643014c9bd2342a619cd649799b746656f
lnsunrmkv <Possibledust11@gmail.com>
2021-12-15 14:58:55 +0300

    Добавьте файлы проекта.

---
+++ 14123isp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14123isp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form1 newForm1 = new Form1();
+            newForm1.Show();
+
+            Form2 newForm2 = new Form2(newForm1);
+            newForm2.Show();
+        }
+    }
+}
